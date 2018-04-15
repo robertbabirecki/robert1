9d5f45c5413258b79af48f19c840a8f7440a806a
Robert <babireckirobert@gmail.com>
2018-04-15 16:00:58 +0200

    1.0 funkcje

---
+++ nowy/venv/funkcje.py
@@ -0,0 +1,15 @@
+def dodawanie(x,y):
+    WynikDodawania=x+y
+    print(WynikDodawania)
+    return WynikDodawania
+
+def odejmowanie(x,y):
+    WynikOdejmowania=x-y
+    print(WynikOdejmowania)
+    return WynikOdejmowania
+x=2
+y=3
+
+dodawanie(x,y)
+odejmowanie(x,y)
+
--- nowy/venv/srypt.py
+++ nowy/venv/srypt.py
@@ -1,2 +1,1 @@
-import listy
-
+import funkcje
