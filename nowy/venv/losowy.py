43a5e4f124fa0e1e509dec3c5363981b434527c4
Robert <babireckirobert@gmail.com>
2018-04-15 17:24:12 +0200

    gra

--- nowy/venv/losowy.py
+++ nowy/venv/losowy.py
@@ -1,7 +1,35 @@
 import random
 import time
 random.seed(time.time())
-a = random.randrange(100)
-print(a)
-imiona=['Robert','Jacek','Kamila','Wacek','Piotr','Anna','Franek','Kaska','Norbert','Bartosz']
-print(random.choice(imiona))
+imiona=['papier','kamien','nozyce']
+x=(random.choice(imiona))
+print('podaj : papier ,kamien,nozyce')
+y=input()
+if x == y:
+     print('jest remis')
+if y == 'papier' and x == 'kamien':
+    print('komputer wybrał:')
+    print(x)
+    print('wygrywsz')
+if y == 'papier' and x == 'nozyce':
+    print('komputer wybrał:')
+    print(x)
+    print('przegrywasz')
+if y == 'kamien' and x == 'papier':
+    print('komputer wybrał:')
+    print(x)
+    print('przegrywasz')
+if y == 'kamien' and x == 'nozyce':
+    print('komputer wybrał:')
+    print(x)
+    print('wygrywasz')
+if y == 'nozyce' and x == 'papier':
+    print('komputer wybrał:')
+    print(x)
+    print('wygrywasz')
+if y == 'nozyce' and x == 'kamien':
+    print('komputer wybrał:')
+    print(x)
+    print('przegrywasz')
+
+
