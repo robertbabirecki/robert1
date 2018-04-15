9b4a99345211872097c223c76994a69187b95c84
Robert <babireckirobert@gmail.com>
2018-04-15 17:40:33 +0200

    gra1.1

--- nowy/venv/losowy.py
+++ nowy/venv/losowy.py
@@ -3,33 +3,40 @@ import time
 random.seed(time.time())
 imiona=['papier','kamien','nozyce']
 x=(random.choice(imiona))
-print('podaj : papier ,kamien,nozyce')
-y=input()
-if x == y:
-     print('jest remis')
-if y == 'papier' and x == 'kamien':
-    print('komputer wybrał:')
-    print(x)
-    print('wygrywsz')
-if y == 'papier' and x == 'nozyce':
-    print('komputer wybrał:')
-    print(x)
-    print('przegrywasz')
-if y == 'kamien' and x == 'papier':
-    print('komputer wybrał:')
-    print(x)
-    print('przegrywasz')
-if y == 'kamien' and x == 'nozyce':
-    print('komputer wybrał:')
-    print(x)
-    print('wygrywasz')
-if y == 'nozyce' and x == 'papier':
-    print('komputer wybrał:')
-    print(x)
-    print('wygrywasz')
-if y == 'nozyce' and x == 'kamien':
-    print('komputer wybrał:')
-    print(x)
-    print('przegrywasz')
+y=0
+print('"koniec"  konczy gre')
+while y !='koniec':
+    print('podaj : papier ,kamien,nozyce')
+
+    y=input()
+
+    if x == y:
+        print('komputer wybrał')
+        print(x)
+        print('jest remis')
+    if y == 'papier' and x == 'kamien':
+        print('komputer wybrał:')
+        print(x)
+        print('wygrywsz')
+    if y == 'papier' and x == 'nozyce':
+        print('komputer wybrał:')
+        print(x)
+        print('przegrywasz')
+    if y == 'kamien' and x == 'papier':
+        print('komputer wybrał:')
+        print(x)
+        print('przegrywasz')
+    if y == 'kamien' and x == 'nozyce':
+        print('komputer wybrał:')
+        print(x)
+        print('wygrywasz')
+    if y == 'nozyce' and x == 'papier':
+        print('komputer wybrał:')
+        print(x)
+        print('wygrywasz')
+    if y == 'nozyce' and x == 'kamien':
+        print('komputer wybrał:')
+        print(x)
+        print('przegrywasz')
 
 
