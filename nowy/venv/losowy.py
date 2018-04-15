e4caadf9f7548c2030acf479bd16d5c0f77ae592
Robert <babireckirobert@gmail.com>
2018-04-15 16:50:34 +0200

    los

---
+++ nowy/venv/losowy.py
@@ -0,0 +1,7 @@
+import random
+import time
+random.seed(time.time())
+a = random.randrange(100)
+print(a)
+imiona=['Robert','Jacek','Kamila','Wacek','Piotr','Anna','Franek','Kaska','Norbert','Bartosz']
+print(random.choice(imiona))
